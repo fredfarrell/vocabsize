cefa215ca569856d540e46db6919b261215a62a2
Fred <Fred Farrell>
2016-11-26 10:31:01 +0000

    Fixed estimation algorithm (much simpler and correct!) Added user input.

--- main.py
+++ main.py
@@ -28,13 +28,8 @@ def scipy_max_likelihood(ps,pu):
 
 	init_guess = 1 / (np.min(ps))
 
-	return optimize.fmin(f,1)[0]
+	return optimize.fmin(f,init_guess,disp=False)[0]
 
-def new_word_prob(pi,m):
-	'''Probability that the mth word you see is new to you,
-	given a frequency table pi.'''
-    
-	return np.sum(pi*((1-pi)**m))
 
 class VocabSizeEstimator:
 	'''Class to extimate your vocab size based on words you do and don't know and 
@@ -43,18 +38,16 @@ class VocabSizeEstimator:
 	def __init__(self,corpus_path):
 		'''self.Corpus should be a csv file with a list of word, relative frequency.'''
 
+		print('Initializing...')
+
 		#load in corpus
 		self.corpus = pd.read_csv(corpus_path, header=False)
 		self.corpus.columns = ['word','freq']
 
-		#use corpus to calculate words seen - words known conversion
+		#calculate unique word conversion from words seen
 		#need to approximate to get to big enough numbers
-		probs = np.array(self.corpus['freq'])
+		self.probs = np.array(self.corpus['freq'])
 
-		new_words = np.array([ new_word_prob(probs,m) for m in range(10000) ])
-		sampled_new_words = np.array([ new_word_prob(probs,m) for m in range(0,50000000,10000) ])
-
-		self.words_known_sampled = sampled_new_words.cumsum()*10000 + new_words.sum()
 
 	def estimate_words_seen(self,sample,known):
 		'''Get estimate of total words seen, given a sample of words (dataframe) and
@@ -69,10 +62,19 @@ class VocabSizeEstimator:
 	def estimate_words_known_corpus(self, words_seen):
 		'''Now we need to go from words seen to words known, i.e. *unique* words seen.
 		Here we base it on the frequencies of words in the self.corpus. Note that this assumes
-		there are no other words, so the estimate is bounded by the size of the self.corpus freq table.'''
+		there are no other words, so the estimate is bounded by the size of the self.corpus freq table.
+		[The derivation of this equation is a little involved]'''
 
-		words_seen_nearest_10k = int(words_seen / 10000)
-		return int(self.words_known_sampled[words_seen_nearest_10k])
+		s = np.vectorize(lambda x: (x-1)*((1-x)**words_seen-1))
+
+		return int(np.sum(s(self.probs)))
+
+	def estimate_words_known_theoretical(self,words_seen):
+		'''Calculate unique words seen using a theoretical distribution of word frequencies.
+		Takes parameters A and alpha describing the frequency ditribution:
+		f(i) = A/i^alpha where i is the rank of a word in frequency.'''
+
+		pass 
 
 	def generate_sample(self,size,rare_threshold):
 		'''Generate a sample of rare words from the self.corpus list.'''
@@ -80,6 +82,37 @@ class VocabSizeEstimator:
 		rare_words = self.corpus[self.corpus['freq']<rare_threshold]
 		rows = random.sample(rare_words.index,size)
 		return rare_words.loc[rows]
+
+	def get_vocab_size(self,sample,known):
+		'''Get vocab size estimate based on knowledge of sample of words.'''
+
+		return self.estimate_words_known_corpus(self.estimate_words_seen(sample,known))
+
+	def run(self):
+
+		sample = self.generate_sample(20,1e-7)
+
+		K=[]
+
+		for i,word in enumerate(sample['word']):
+			print(word)
+
+			while True:
+
+				know = raw_input('Do you know this word y/n? ')
+
+				if know == 'y':
+					K.append(1)
+					break
+				elif know == 'n':
+					K.append(0)
+					break
+				else:
+					continue
+
+		print 'OK, estimateing vocabulary size...'
+		print 'Your vocabulary size is ', self.get_vocab_size(sample,K)
+
 
 
 
